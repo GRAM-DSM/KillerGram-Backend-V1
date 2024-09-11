5f00230d9d92b3be5e177fe21dfa5f28e5e7766f
나병현 <nbhyun0329@gmail.com>
2024-09-11 20:14:58 +0900

    #1 feat :: 프로젝트 생성

---
+++ src/main/java/gram/killergram/KillerGramApplication.java
@@ -0,0 +1,13 @@
+package gram.killergram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KillerGramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KillerGramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gram/killergram/KillerGramApplicationTests.java
@@ -0,0 +1,13 @@
+package gram.killergram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KillerGramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
