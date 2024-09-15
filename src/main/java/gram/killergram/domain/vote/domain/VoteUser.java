d599b2c74ae39f67ddea96d388d746433b698a28
나병현 <nbhyun0329@gmail.com>
2024-09-15 15:10:22 +0900

    feat :: vote user 엔티티 생성(#12)

---
+++ src/main/java/gram/killergram/domain/vote/domain/VoteUser.java
@@ -0,0 +1,24 @@
+package gram.killergram.domain.vote.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity(name = "vote_user")
+@Getter
+@NoArgsConstructor
+@Table(name = "vote_user_tbl")
+public class VoteUser {
+
+    @Id
+    @Column(name = "vote_student",unique = true, nullable = false)
+    private UUID voteStudent;
+
+    @Column(name = "is_attend", nullable = false)
+    private boolean isAttend;
+}
