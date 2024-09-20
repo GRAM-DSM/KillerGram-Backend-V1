cdf0467257ed01f0c2594670e64a500a4dcef0f9
hy2n <github1@ncloud.sbs>
2024-09-20 15:08:53 +0900

    feature : Student 엔티티를 바탕으로, 서비스 코드를 작성하였습니다.

---
+++ src/main/java/gram/killergram/domain/user/controller/dto/request/UserLoginRequest.java
@@ -0,0 +1,21 @@
+package gram.killergram.domain.user.controller.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginRequest {
+
+    @NotBlank
+    @Size(max = 30)
+    private String accountId;
+
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,16}$")
+    private String password;
+}
---
+++ src/main/java/gram/killergram/domain/user/domain/service/UserLoginService.java
@@ -0,0 +1,4 @@
+package gram.killergram.domain.user.domain.service;
+
+public class UserLoginService {
+}
